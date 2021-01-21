1aa5a9cdac04fab1c9f42dfb56ab1a6d15b2d7d0
filipwtf <37674069+filipwtf@users.noreply.github.com>
2021-01-21 15:54:37 +0000

    Meta tags

--- src/sharex.js
+++ src/sharex.js
@@ -3,40 +3,75 @@ require('dotenv').config();
 const upload = require('express-fileupload');
 const randomstring = require('randomstring');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-// your domain
-const domain = 'localhost:5000';
+const PORT = process.env.PORT || 5000;
+const DOMAIN = process.env.DOMAIN || 'localhost:' + PORT;
+const SECURE = 'http';
 
-//Serve static files from ./files directory
-app.use(express.static('files'));
-
-//Specify file upload limit 100mb (may need to change on cloudflare/nginx)
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use('/static', express.static('files'));
 app.use(upload({ limits: { fileSize: 1000 * 1000 * 100 } }));
 app.use(express.json());
 
-app.post('/upload', (req, res) => {
+app.post('/upload', async (req, res) => {
   if (req.body.key === process.env.KEY) {
     const file = req.files.file;
     const randomName = randomstring.generate({ length: 12 });
     const type = file.name.split('.');
 
-    const fileName = randomName + '.' + type[type.length - 1];
+    const fileName = await generateFileName(randomName, type);
     file.mv(`./files/${fileName}`, (err) => {
       if (err) return res.status(500).send(err);
-      res.send(`http://${domain}/${fileName}`);
+      res.send(`${SECURE}://${DOMAIN}/${fileName}`);
     });
   } else {
     res.sendStatus(418);
   }
 });
 
-// Catch all
-app.get('*', (req, res) => {
+app.get('/:file', async (req, res) => {
+  const file = req.params.file;
+  const description = fs.stat(`./static/${file}`, (err, fileStats) => {
+    if (err) {
+      return 'N|A';
+    } else {
+      return convertBytes(fileStats.size);
+    }
+  });
+  res.render('index', {
+    title: DOMAIN,
+    domain: DOMAIN,
+    url: `${SECURE}://${DOMAIN}/`,
+    description: description,
+    image: `${SECURE}://${DOMAIN}/static/${file}`,
+  });
+});
+
+app.get('*', async (req, res) => {
   res.sendStatus(418);
 });
 
-const PORT = process.env.PORT || 5000;
+const generateFileName = async (str, type) => {
+  return str + '.' + type[type.length - 1];
+};
+
+const convertBytes = (bytes) => {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+  if (bytes == 0) {
+    return 'n/a';
+  }
+
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  if (i == 0) {
+    return bytes + ' ' + sizes[i];
+  }
+
+  return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+};
 
 app.listen(PORT, () => {
   console.log(`Image uploader listening on port: ${PORT}`);
